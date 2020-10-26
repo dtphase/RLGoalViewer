1384375c7d95e49495ecccba7e121b3029982b2e
Gambit4k <gambit4kyt@gmail.com>
2020-10-26 13:14:15 +1300

    Extracted environment variables to seperate file and create example env file.

---
+++ ~env.py
@@ -0,0 +1,6 @@
+SITE = "https://ballchasing.com/"
+REPLAYS_FOLDER = "C:/Users/example/Documents/My Games/Rocket League/TAGame/Demos/"
+TOKEN = "example_token"
+HEADERS = {"Authorization": TOKEN}
+REPLAY_EXTENSION = ".replay"
+PLAYLIST = {"unranked-duels", "unranked-doubles", "unranked-standard", "ranked-duels", "ranked-doubles", "ranked-solo-standard", "ranked-standard", "private"}
